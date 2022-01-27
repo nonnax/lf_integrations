81ef449a16efed932a7f76a62107a177f704df5c
nonnax <ironald@gmail.com>
2022-01-27 11:01:09 +0800

    update: new.rb

--- bin/new.rb
+++ bin/new.rb
@@ -5,6 +5,11 @@ TEMPLATE = <<~DOC
   #!/usr/bin/env ruby
   # Id$ nonnax #{Time.now}
 DOC
-File.open(ARGV.first || 'noname.rb', 'w') do |f|
+fname=ARGV.first || 'noname.rb'
+File.open(fname, 'w') do |f|
   f.puts TEMPLATE
 end
+File.chmod(0755, fname)
+
+cmd="lf -remote 'send select #{fname}'"
+IO.popen(cmd, &:read)
