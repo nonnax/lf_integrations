ef02ca9b51336c2354be828174fc7888b25d1d62
nonnax <ironald@gmail.com>
2022-02-02 16:10:47 +0800

    update: ren.rb

--- bin/ren.rb
+++ bin/ren.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 # Id$ nonnax 2022-01-27 10:59:58 +0800
-ARGF.each do |l|
-  p l
+require 'fileutils'
+
+ARGF.readlines(chomp: true).each do |l|
+  f_old, f_new = l.split("\t")
+  FileUtils.mv f_old, f_new
 end
