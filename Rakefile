b54dc96d541f9ad9200c3cfdf7988c66bc840b54
nonnax <ironald@gmail.com>
2023-02-18 21:22:16 +0800

    add mountdrives.rb util

--- Rakefile
+++ Rakefile
@@ -7,11 +7,11 @@ task :bundle do
 end
 
 desc "Build the lf_integrations gem"
-task build: %w[bundle]  do
+task :build  do
   sh "gem build lf_integrations.gemspec"
 end
 
 desc "install lf_integrations-0.0.1.gem"
 task install: %w[build] do
-  sh "sudo gem install lf_integrations-0.0.1.gem"
+  sh "gem install lf_integrations-0.0.1.gem"
 end
---
+++ bin/mountdrives.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# Id$ nonnax 2023-02-18 21:14:15
+# NAME   MAJ:MIN RM   SIZE RO TYPE MOUNTPOINT
+# sda      8:0    0   1.8T  0 disk
+# ├─sda1   8:1    0   512M  0 part
+# ├─sda2   8:2    0   891G  0 part /home
+# ├─sda3   8:3    0   7.2G  0 part
+# ├─sda4   8:4    0 839.5G  0 part
+# ├─sda5   8:5    0     1M  0 part
+# ├─sda6   8:6    0  93.1G  0 part /
+# └─sda7   8:7    0  31.7G  0 part
+# sdb      8:16   0 931.5G  0 disk
+# ├─sdb1   8:17   0   512M  0 part
+# ├─sdb2   8:18   0     1K  0 part
+# ├─sdb3   8:19   0  41.5G  0 part
+# ├─sdb5   8:21   0 441.5G  0 part
+# └─sdb6   8:22   0   440G  0 part
+
+
+%w[sda3 sda6 sda7 sdb3 sdb5 sdb6 sdc1]
+.map do |name|
+  cmd=format "udisksctl mount -b /dev/%s", name
+end
+.map{|cmd|
+ IO.popen(cmd, &:read)
+}
